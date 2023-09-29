0cdc17737d5fe38e2b758a18bf2cd231a4774bbe
unknown <male.b.t.2000@gmail.com>
2023-09-29 08:46:05 -0300

    Crear tarea final - sin probar (controlador y ruta)

---
+++ api/app/Http/Controllers/TareasController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tareas;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TareasController extends Controller
+{
+    public function CrearTarea(Request $request){
+        $validator = Validator::make($request->all(), [
+            'titulo' => 'required | string',
+            'contenido' => 'required | string',
+            'estado' => 'nullable | string',
+            'autor' => 'nullable | string'
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        return $this->GuardarTarea($request);
+    }
+
+    private function GuardarTarea(Request $request) {
+        $nuevaTarea = new tareas();
+        $nuevaTarea -> titulo = $request->input('titulo');
+        $nuevaTarea -> contenido = $request->input('contenido');
+        $nuevaTarea -> estado = $request->input('estado');
+        $nuevaTarea -> autor = $request->input('autor');
+        $nuevaTarea -> save();
+            
+        return $nuevaTarea;
+    }
+}
